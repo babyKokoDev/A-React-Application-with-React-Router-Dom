fb7daf6e57b9e161d451be435227fa30d302930e
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-04 00:50:05 +0100

    hero section added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import Hero from './Hero'
 import './HomeStyles.css'
 import NavBar from './NavBar'
 
 const Home = () => {
   return (
+    <>
     <NavBar />
+    <Hero />
+    </>
   )
 }
 
