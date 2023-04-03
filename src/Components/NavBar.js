7545f0d6de3929b543c675b05e8c98e92b649b59
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-03 22:44:31 +0100

    first commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {link} from 'react-router-dom'
+
+import {FaBars, FaTimes} from 'react-icons/fa'
+
+const NavBar = () => {
+  return (
+        <header>
+            <nav className='navbar'>
+
+            </nav>
+        </header>
+  )
+}
+
+export default NavBar
