b246873c029aefe70724252f534722a30962de02
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-04-05 23:16:58 +0100

    Pricing section added

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,12 +2,14 @@ import React from 'react'
 import Hero from './Hero'
 import './HomeStyles.css'
 import NavBar from './NavBar'
+import Pricing from './Pricing'
 
 const Home = () => {
   return (
     <>
     <NavBar />
     <Hero />
+    <Pricing />
     </>
   )
 }
